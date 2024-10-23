5051c77abe8b9ba8aa56a00f455a4745af3f7044
jakubwisniewski <jakub.wisniewski@supermetrics.com>
2024-10-23 10:40:44 +0200

    solution

--- src/main/java/rewards/JdbcBootApplication.java
+++ src/main/java/rewards/JdbcBootApplication.java
@@ -1,13 +1,26 @@
 package rewards;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class JdbcBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JdbcBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcBootApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+        String QUERY = "SELECT count(*) FROM T_ACCOUNT";
+
+        return args -> System.out.println("Hello there. We have "
+                + jdbcTemplate.queryForObject(QUERY, Long.class)
+                + " accounts"
+        );
+    }
 
 }
--- src/test/java/rewards/JdbcBootApplicationTests.java
+++ src/test/java/rewards/JdbcBootApplicationTests.java
@@ -1,13 +1,29 @@
 package rewards;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JdbcBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    public static final String QUERY = "SELECT count(*) FROM T_ACCOUNT";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testNumberOfAccounts() {
+        long count = jdbcTemplate.queryForObject(QUERY, Long.class);
+
+        assertThat(count).isEqualTo(21L);
+    }
 
 }
